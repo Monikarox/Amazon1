dfe1bc1f43f0887503f15213a89dac807b613fde
Monika Sahu <Monika_Sahu@epam.com>
2019-11-18 18:40:59 +0530

    changes

--- src/test/java/basetest/HeadLessBase.java
+++ src/test/java/basetest/HeadLessBase.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
