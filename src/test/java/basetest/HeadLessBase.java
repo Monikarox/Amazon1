b5a06d9b96ff042126fe9ab2eb7d2d8beb19e5fa
Monika Sahu <Monika_Sahu@epam.com>
2019-11-19 11:15:12 +0530

    changes

--- src/test/java/basetest/HeadLessBase.java
+++ src/test/java/basetest/HeadLessBase.java
@@ -20,61 +20,51 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import utilities.PropertyFile;
 
 public class HeadLessBase extends ExtentReportsBaseTest {
-protected WebDriver driver;
-	
+	protected WebDriver driver;
 
-@BeforeTest
-@Parameters({"browser"}) 
+	@BeforeTest
+	@Parameters({ "browser" })
 	public void setUpF(@Optional String browser) throws Exception {
-	
-	String driverChrome = PropertyFile.getProperty("driverC"),pathChrome = PropertyFile.getProperty("pathC");
-	
 
+		String driverChrome = PropertyFile.getProperty("driverC"), pathChrome = PropertyFile.getProperty("pathC");
 
-	if(browser!=null)
-	if (browser.equalsIgnoreCase("firefox")) {
+		if (browser != null)
+			if (browser.equalsIgnoreCase("firefox")) {
 
-		 System.setProperty(PropertyFile.getProperty("driverF"), PropertyFile.getProperty("pathF")); 
-		 FirefoxOptions options =new FirefoxOptions();
-		 options.setHeadless(true);
-		 driver = new FirefoxDriver(options);
-		 System.out.println(" \n FireFox HeadLess Browser invoked");
-		
-		
-	}
-	else if(browser.equalsIgnoreCase("chrome")) {
-	
-		System.setProperty(driverChrome, pathChrome);
-	    ChromeOptions chromeOptions = new ChromeOptions();
-	    chromeOptions.setHeadless(true);
-	    driver = new ChromeDriver(chromeOptions);
-        System.out.println(" \n Chrome HeadLess Browser invoked");
+				System.setProperty(PropertyFile.getProperty("driverF"), PropertyFile.getProperty("pathF"));
+				FirefoxOptions options = new FirefoxOptions();
+				options.setHeadless(true);
+				driver = new FirefoxDriver(options);
+				System.out.println(" \n FireFox HeadLess Browser invoked");
 
-	}
+			} else if (browser.equalsIgnoreCase("chrome")) {
+
+				System.setProperty(driverChrome, pathChrome);
+				ChromeOptions chromeOptions = new ChromeOptions();
+				chromeOptions.setHeadless(true);
+				driver = new ChromeDriver(chromeOptions);
+				System.out.println(" \n Chrome HeadLess Browser invoked");
+
+			}
 //	else if(browser.equalsIgnoreCase("htmlunit")) {
 //		
 //		 System.setProperty(PropertyFile.getProperty("driverC"), PropertyFile.getProperty("pathC"));
 //		 driver=new HtmlUnitDriver();
 //
 //	}
-				
-	else {
-		throw new Exception("Browser not correct");
-		}
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	
-	
-     driver.get(PropertyFile.getProperty("url"));
-     System.out.println("Title of page is: "+driver.getTitle());
-	
+
+			else {
+				throw new Exception("Browser not correct");
+			}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get(PropertyFile.getProperty("url"));
+		System.out.println("Title of page is: " + driver.getTitle());
+
 	}
-	
-
-
 
 	@AfterSuite(alwaysRun = true)
-	public void tearDown() throws Exception
-	{
-		//Sdriver.quit();
+	public void tearDown() throws Exception {
+		// Sdriver.quit();
 	}
 }
